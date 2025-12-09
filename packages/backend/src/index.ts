e0ce8b5bd1eb3997c2446ebd294ff50605b094f3
mick811 <mikkelelvers@outlook.com>
2025-12-09 21:24:14 +0100

    setup basic turborepo

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+    return c.json({ message: "Hello Hono!" });
+});
+
+const port = 3000;
+console.log(`server is running on port ${port}`);
+
+serve({
+    fetch: app.fetch,
+    port,
+})
